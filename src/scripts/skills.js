d1deeeafa974bdab8a730bf10d2db5fd319aeab6
Alexey Orlov <orelkv@mail.ru>
2020-05-10 01:07:33 +0300

    Skills готовы

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,6 +1,45 @@
-import Vue from vue;
+import Vue from 'vue';
 
-new Vue ({
-  el: '#skills',
-  template: '#skills'
-})
+const skill = {
+  template: '#skill',
+  props: ['skill'],
+  methods: {
+    drawCircle() {
+      const circle = this.$refs['circle'];
+      const dashArray = parseInt(getComputedStyle(circle).getPropertyValue('stroke-dasharray'));
+      const percent = (dashArray/100)*(100-this.skill.percent);
+      circle.style.strokeDashoffset = percent;
+    }
+  },
+  mounted() {
+    this.drawCircle()
+  }
+};
+
+const skillsRow = {
+  template: "#skills-row",
+  components: {
+    skill
+  },
+  props: ['category']
+};
+
+
+
+new Vue({
+  el: '#skills-tech',
+  template: '#skills-components',
+  components: {
+    skillsRow
+  },
+
+  data() {
+    return {
+      skills: []
+    }
+  },
+  created() {
+    const data = require('../data/skills.json');
+    this.skills = data;
+  }
+});

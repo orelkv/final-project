3184f8dd8236c0b12e82841de2d2bf269c5fe4eb
Alexey Orlov <orelkv@mail.ru>
2020-05-25 21:52:46 +0300

    админка роутер настроен

--- src/main.js
+++ src/main.js
@@ -3,3 +3,10 @@ if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
 
+import './scripts/comments'
+import './scripts/form'
+import './scripts/menu'
+import './scripts/overlay'
+import './scripts/parallax'
+import './scripts/skills'
+import './scripts/works'
